70f1914b1066bf382ef7014e6038908c36cbf87a
Qudratillo8373 <80778542+Qudratillo8373@users.noreply.github.com>
2022-03-22 13:52:08 -0500

    fix issue

--- vpc.tf
+++ vpc.tf
@@ -1,28 +1,4 @@
 resource "aws_vpc" "main" {
   cidr_block = var.cidr_block
-  tags   = var.tags
-}
-
-
-resource "aws_subnet" "private1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = private_subnets_cidr[0]
-}
-
-
-variable "private_subnets_cidr" {
-    type = "list"
-}
-
-
-
-resource "aws_subnet" "private2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = private_subnets_cidr[1]
-}
-
-
-resource "aws_subnet" "private3" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = private_subnets_cidr[2]
+  tags   = var.tags 
 }
