9fda28d0fb5e837ec5435806cbb440b307d5d4e8
Qudratillo8373 <80778542+Qudratillo8373@users.noreply.github.com>
2022-03-22 13:57:47 -0500

    change space

--- private_subnet.tf
+++ private_subnet.tf
@@ -2,14 +2,10 @@ resource "aws_subnet" "private1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = private_subnets_cidr[0]
 }
-
-
 resource "aws_subnet" "private2" {
   vpc_id     = aws_vpc.main.id
   cidr_block = private_subnets_cidr[1]
 }
-
-
 resource "aws_subnet" "private3" {
   vpc_id     = aws_vpc.main.id
   cidr_block = private_subnets_cidr[2]
