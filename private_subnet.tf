7de727e0b78d9de30b7edb7236d2ceecd5b88f6d
Qudratillo8373 <80778542+Qudratillo8373@users.noreply.github.com>
2022-03-21 19:26:45 -0500

    added subnets

--- private_subnet.tf
+++ private_subnet.tf
@@ -1,19 +1,7 @@
-resource "aws_vpc" "main" {
-  cidr_block = var.cidr_block
-  tags   = var.tags
-}
-
-
 resource "aws_subnet" "private1" {
   vpc_id     = aws_vpc.main.id
   cidr_block = private_subnets_cidr[0]
 }
-
-
-variable "private_subnets_cidr" {
-    type = "list"
-}
-
 
 
 resource "aws_subnet" "private2" {
@@ -25,4 +13,3 @@ resource "aws_subnet" "private2" {
 resource "aws_subnet" "private3" {
   vpc_id     = aws_vpc.main.id
   cidr_block = private_subnets_cidr[3]
-}
