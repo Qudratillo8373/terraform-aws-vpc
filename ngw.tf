f854756870ad990c86f1ba67b16771fb8d3dd7b5
Qudratillo8373 <80778542+Qudratillo8373@users.noreply.github.com>
2022-03-23 18:02:46 -0500

    change ngw

--- ngw.tf
+++ ngw.tf
@@ -1,14 +1,6 @@
 resource "aws_eip" "example" {
-
-allocate_id = aws_eip.example.id
-subnet_id  = aws_subnet.example.id
-     vpc     = true 
-
-        tags  = {
-            Name = "Dev"
-        }
-
-
-
+    allocate_id = aws_eip.example.id
+    subnet_id  = aws_subnet.example.id
+    vpc     = true 
     depends_on = [aws_internet_gateway.example]
 }
