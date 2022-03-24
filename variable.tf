3db45f9194669125d799c90188b3851d4fafd48e
Qudratillo8373 <80778542+Qudratillo8373@users.noreply.github.com>
2022-03-23 22:27:54 -0500

    fix issue

--- variable.tf
+++ variable.tf
@@ -13,13 +13,13 @@ variable "tags"{
 variable "private_subnets_cidr" {
     description = "Please provide a CIDR block for private to build a VPC"
     type = list
-    default = { [
+    default = ([
         "10.0.1.0/24",
         "10.0.2.0/24",
         "10.0.3.0/24",
-    ]
+    ])
     }
-}
+
 
 variable "pubic_subnets_cidr" {
     description = "Please provide a CIDR block for public to build a VPC"
